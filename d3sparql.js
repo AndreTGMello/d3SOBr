297a01f8b942e6b9b1101d4444c3f584abd56ffb
AndreTGMello <andre.tgmello@gmail.com>
2016-12-12 00:38:31 -0200

    Settimeout test

--- d3sparql.js
+++ d3sparql.js
@@ -56,17 +56,14 @@ var d3sparql = {
     </html>
 */
 d3sparql.query = function(endpoint, sparql, callback) {
-  /* Modified */
-  var json;
   var url = endpoint + "?query=" + encodeURIComponent(sparql)
   if (d3sparql.debug) { console.log(endpoint) }
   if (d3sparql.debug) { console.log(url) }
   var mime = "application/sparql-results+json"
   d3.xhr(url, mime, function(request) {
-    json = request.responseText
+    var json = request.responseText
     if (d3sparql.debug) { console.log(json) }
     callback(JSON.parse(json))
-    return json;
   })
 /*
   d3.json(url, function(error, json) {
@@ -75,8 +72,6 @@ d3sparql.query = function(endpoint, sparql, callback) {
     callback(json)
   })
 */
-  /* Modified */
-  return json;
 }
 
 /*
